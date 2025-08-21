2bcb1a0491b0bf990b90c125c7dd45ab4d887093
Pranav <23z139@psgitech.ac.in>
2025-08-21 14:51:17 +0530

    Added frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Heading from './components/Heading'
+import InputText from './components/InputText'
+
+function App() {
+    return (
+        <div className='app'>
+            <Heading />
+            <h3>Summary:</h3>
+            <InputText />
+        </div>
+    )
+}
+
+export default App
---
+++ frontend/src/components/Heading.jsx
@@ -0,0 +1,5 @@
+export default function Heading() {
+  return (
+    <h1 className='heading'>AI Text Summarizer</h1>
+  )
+}
